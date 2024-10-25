1793b156f21fb7d2fc1b56b2776de81cb55defcf
A416J <desfannizulya@gmail.com>
2024-10-25 10:09:33 +0700

    Initial commit

---
+++ app/src/main/java/com/fani/klinik_apk/model/ModelIcon.kt
@@ -0,0 +1,5 @@
+package com.fani.klinik_apk.model
+
+data class ModelIcon(
+    var iconImage : Int,
+    var iconName : String )
---
+++ app/src/main/java/com/fani/klinik_apk/model/ModelListDoctors.kt
@@ -0,0 +1,12 @@
+package com.fani.klinik_apk.model
+
+data class ModelListDoctors(
+
+    var NamaDr : String,
+    var ImageDr : Int,
+    var ImageBintang : Int,
+    var BidangDr : String,
+    var totalReview : String,
+    var totalRating : String
+
+)
